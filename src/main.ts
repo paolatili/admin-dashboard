811348a9d526eba680db88f90db3d0822c49861d
Paola Tili <paolatili@Paolas-MacBook-Pro.local>
2024-09-29 17:36:26 +0200

    created the top bar and some stylings

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,23 @@
-import './assets/main.css'
+import './assets/layout/_topbar.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+import StyleClass from 'primevue/styleclass';
+import '@/assets/styles.scss';
 
 const app = createApp(App)
-
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura
+  }
+});
 app.use(createPinia())
+app.directive('styleclass', StyleClass);
 app.use(router)
+
 
 app.mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import AppLayout from '@/layout/AppLayout.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    {
+      path: '/',
+      component: AppLayout,
+      children: [{
+        path: '/',
+        name: "dashboard",
+        component: () => import('@/views/DashboardView.vue')
+      }]
+    }
+  ]
 })
 
 export default router
